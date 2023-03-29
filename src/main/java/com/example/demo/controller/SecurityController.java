5b1caef4c770c77b5add44c3022d44a7371a971b
haruki92 <harukikunihira@gmail.com>
2023-03-30 07:34:05 +0900

    新規会員登録機能作成

--- src/main/java/com/example/demo/controller/SecurityController.java
+++ src/main/java/com/example/demo/controller/SecurityController.java
@@ -1,16 +1,28 @@
 package com.example.demo.controller;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.Authority;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class SecurityController {
+	private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
 
 	@GetMapping("/login")
 	public String login() {
@@ -24,4 +36,50 @@ public class SecurityController {
 
 		return "user";
 	}
+
+	@GetMapping("/register")
+	public String register(User user) {
+		return "register";
+	}
+
+	@PostMapping("/register")
+	public String complete(HttpSession session) {
+		User user = (User) session.getAttribute("user");
+
+		//		パスワードを暗号化してセット
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+		//		その他ユーザ情報をセット
+		user.setCreatedAt(LocalDateTime.now());
+		user.setUpdatedAt(LocalDateTime.now());
+		user.setDeleteFlag(0);
+
+		//		管理者の場合
+		if (user.isAdmin()) {
+			user.setAuthority(Authority.ADMIN); // authorityをadminに
+		} else {
+			//			そうでない場合は
+			user.setAuthority(Authority.USER); // authorityをuserに
+		}
+		//		 ユーザ情報を保存
+		userRepository.save(user);
+
+		return "redirect:/login?register";
+	}
+
+	@GetMapping("/confirm")
+	public String getConfirm(HttpSession session) {
+		return "confirm";
+	}
+
+	@PostMapping("/confirm")
+	public String confirm(@Validated User user, BindingResult result, HttpSession session) {
+		if (result.hasErrors()) {
+			return "register";
+		}
+		session.setAttribute("user", user);
+
+		return "redirect:/confirm";
+	}
+
 }
